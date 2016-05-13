c9c8cdd490eb08f4077351083f893d2231d609ac
Lubomir Host <lubomir.host@gmail.com>
2016-05-13 14:54:52 +0200

    Vim init & bootstrap

--- vim.symlink/install.sh
+++ vim.symlink/install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ ! -d ~/.vim/bundle ]; then
+	echo "~/.vim directory not symlinked to dotfiles.git yet"
+	exit 1
+fi
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
 	 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
