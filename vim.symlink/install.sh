a08da4f5fea8e16bfe0648c1886a5bb60f541f0d
Lubomir Host <lubomir.host@gmail.com>
2016-05-13 15:01:51 +0200

    Fix: vim breaks terminal

--- vim.symlink/install.sh
+++ vim.symlink/install.sh
@@ -8,5 +8,5 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
 	 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
-vim +PluginInstall +qall
+sh -c '</dev/tty vim +PluginInstall +qall'
 
